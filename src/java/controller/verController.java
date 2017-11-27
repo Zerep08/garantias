c809aaaee2a14e2039b8f3cc59184a4280ffab4a
Zerep08 <francisco.pgarcia28@gmail.com>
2017-11-27 16:21:52 -0600

    Front-end agregado

---
+++ src/java/controller/verController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+/**
+ *
+ * @author zerep08
+ */
+@Controller
+@RequestMapping("/ver.htm")
+public class verController {
+    @RequestMapping(method = RequestMethod.GET)
+    public  String otroMetodo(Model model){
+        return "mostrarTicket";
+}
+}
